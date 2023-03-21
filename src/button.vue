d5d33dd9611a7f673b9753a49d1ab5a672f4d446
king-open <kingwang.0624@gmail.com>
2023-03-21 19:30:11 +0800

    添加 click 事件

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,13 @@
 <template>
-   <button class="u-button" :class="{[`icon-${iconPosition}`]: true}">
-    <u-icon class="icon" v-if="icon" :name="icon"/>
-    <u-icon class="loading" name="loading"></u-icon>
+  <button
+    class="u-button"
+    :class="{ [`icon-${iconPosition}`]: true }"
+    @click="$emit('click')"
+  >
+    <u-icon class="icon" v-if="icon && !loading" :name="icon" />
+    <u-icon class="loading" v-if="loading" name="loading"></u-icon>
     <div class="content">
-      <slot/>
+      <slot />
     </div>
   </button>
 </template>
@@ -11,40 +15,71 @@
 <script>
 export default {
   // props: ['icon', 'iconPosition']
-  props:{
-    icon:{},
-    iconPosition:{
+  props: {
+    icon: {},
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    iconPosition: {
       type: String,
-      default: 'left',
-      validator (value) {
-          return value === 'left' || value === 'right'
-        }
-      }
-  }
+      default: "left",
+      validator(value) {
+        return value === "left" || value === "right";
+      },
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-@keyframes spin{
-  0%{transform: rotate(0deg);}
-  100%{transform: rotate(360deg);}
-}
-.u-button {font-size: var(--font-size);height: var(--button-height);
-  padding: 0 1em;border-radius: var(--border-radius);border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  
-  display: inline-flex; justify-content: center; align-items: center;
-    vertical-align: middle;
-    &:hover { border-color: var(--border-color-hover); }
-    &:active { background-color: var(--button-active-bg); }
-    &:focus { outline: none; }
-    > .content { order: 2; }
-    > .icon { order: 1; margin-right: .1em; }
-    &.icon-right {
-      > .content { order: 1; }
-      > .icon { order: 2; margin-right: 0; margin-left: .1em;}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
   }
-  .loading{
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.u-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+  &.icon-right {
+    > .content {
+      order: 1;
+    }
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.1em;
+    }
+  }
+  .loading {
     animation: spin 2s infinite linear;
   }
 }
